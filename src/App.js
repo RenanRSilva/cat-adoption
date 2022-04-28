b165c0e128ada59497ed20a552b594e0cb98feb2
RenanRSilva <renanrambuls@gmail.com>
2022-04-28 16:16:15 -0300

    feat: cards component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import Card from "./components/Card/Card";
-
+import Cards from "./components/Cards/Cards";
+import cats from "./mocks/cats.json"
 function App() {
   return (
-    <div className="App">
-      <Card
-        name="Sydney"
-        phone="111-111-1111"
-        email="laith@hotmail.com"
-        image={{
-          url: "https://media.istockphoto.com/photos/kitten-exploring-domestic-setting-picture-id1335433001?b=1&k=20&m=1335433001&s=170667a&w=0&h=9XToSFW4X8uO4QJKBdwFbKJjgxjXiaizLyULFrb4_Gc=",
-          alt: "cute cat"
-        }}
-        favoured={false}
-      />
+    <div>
+      <Cards cats={cats} />
     </div>
   );
 }
